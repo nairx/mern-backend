dd5758275608edf5630723116ad0d18d7b8554f2
nairx <nairxprv@gmail.com>
2026-02-24 11:52:32 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
 const addUser = async (req, res) => {
-  const result = await userModel.create(req.body);
+  const body = req.body;
+  const hashPassword = await bcrypt.hash(body.password, 10);
+  body.password = hashPassword;
+  const result = await userModel.create(body);
   res.json(result);
 };
 const showUsers = async (req, res) => {
@@ -12,4 +16,18 @@ const deleteUser = async (req, res) => {
   const result = await userModel.findByIdAndDelete(id);
   res.json(result);
 };
-export { addUser, showUsers, deleteUser };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const found = await userModel.findOne({ email });
+  if (found) {
+    const chkPassword = await bcrypt.compare(password, found.password);
+    if (chkPassword) {
+      res.status(200).json({ message: "Success" });
+    } else {
+      res.status(401).json({ message: "Invalid Password" });
+    }
+  } else {
+    res.status(401).json({ message: "User not found" });
+  }
+};
+export { addUser, showUsers, deleteUser, login };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,14 @@
 import {
   addUser,
   deleteUser,
+  login,
   showUsers,
 } from "../controllers/userController.js";
 import express from "express";
 const userRouter = express.Router();
 
 userRouter.get("/showUsers", showUsers);
+userRouter.post("/login", login);
 userRouter.post("/addUser", addUser);
 userRouter.delete("/deleteUser/:id", deleteUser);
 
