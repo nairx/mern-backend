063d77aeb7b49909640e4fb47642ec0a9fc2f7a0
nairx <nairxprv@gmail.com>
2026-02-24 11:57:45 +0530

    updated

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken"
+const authenticate = (req,res,next) => {
+// const authHeader = req.headers.authorization
+// const token = authHeader.split(" ")[1]
+// const user = jwt.verify(token,SECRET)
+}
+
+// const authorize = (req,res,next) => {
+
+// }
